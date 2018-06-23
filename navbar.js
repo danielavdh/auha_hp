0c92fbad3dec2abd37aca15f94eefcb3b3cf3c02
Aurelia Hammerli <aurelia@hammerli.co.uk>
2018-06-23 16:49:17 +0100

    menu finished

--- navbar.js
+++ navbar.js
@@ -1,7 +1,36 @@
 var hermione={
-		navbar: '<ul>\
-								<li><a href="index.html">home</a></li>\
-								<li><a href="about.html">about us</a></li>\
-								<li><a href="contact.html">contact us</a></li>\
-						 </ul>'
+		navbar: '<div id="menu">\
+			<a href="/">\
+				<img src="images/home.jpg" />\
+			</a>\
+			<a href="books/books.html">\
+				<img src="images/book1.jpg" />\
+			</a>\
+			<a href="houses/houses.html">\
+				<img src="images/crest.png" />\
+			</a>\
+			<a href="folk/folk.html">\
+				<img src="images/ginny.jpg" />\
+			</a>\
+			<a href="creatures/creatures.html">\
+				<img src="images/fawkes.jpg" />\
+			</a>\
+		</div>',
+		navbar1: '<div id="menu">\
+			<a href="/">\
+				<img src="../images/home.jpg" />\
+			</a>\
+			<a href="../books/books.html">\
+				<img src="../images/book1.jpg" />\
+			</a>\
+			<a href="../houses/houses.html">\
+				<img src="../images/crest.png" />\
+			</a>\
+			<a href="../folk/folk.html">\
+				<img src="../images/ginny.jpg" />\
+			</a>\
+			<a href="../creatures/creatures.html">\
+				<img src="../images/fawkes.jpg" />\
+			</a>\
+		</div>'
 }
