4fd13bea7e781385106a68fa0e68e05fb15f43b5
Aurelia Hammerli <aurelia@hammerli.co.uk>
2018-06-25 19:11:13 +0100

    title tags

--- navbar.js
+++ navbar.js
@@ -2,31 +2,39 @@ var hermione={
 		navbar: '<a href="/">\
 				<img src="images/home.jpg" />\
 			</a>\
-			<a href="books/">\
+			<a href="books/"\
+				 title="books">\
 				<img src="images/book1.jpg" />\
 			</a>\
-			<a href="houses/">\
+			<a href="houses/"\
+				 title="houses">\
 				<img src="images/crest.png" />\
 			</a>\
-			<a href="folk/">\
+			<a href="folk/"\
+				 title="wizards and witches">\
 				<img src="images/ginny.jpg" />\
 			</a>\
-			<a href="creatures/">\
+			<a href="creatures/"\
+				 title="magical creatures">\
 				<img src="images/fawkes.jpg" />\
 			</a>',
 		navbar1: '<a href="/">\
 				<img src="../images/home.jpg" />\
 			</a>\
-			<a href="../books/">\
+			<a href="../books/"\
+				 title="books">\
 				<img src="../images/book1.jpg" />\
 			</a>\
-			<a href="../houses/">\
+			<a href="../houses/"\
+				 title="houses">\
 				<img src="../images/crest.png" />\
 			</a>\
-			<a href="../folk/">\
+			<a href="../folk/"\
+				 title="wizards and witches">\
 				<img src="../images/ginny.jpg" />\
 			</a>\
-			<a href="../creatures/">\
+			<a href="../creatures/"\
+				 title="magical creatures">\
 				<img src="../images/fawkes.jpg" />\
 			</a>'
 }
